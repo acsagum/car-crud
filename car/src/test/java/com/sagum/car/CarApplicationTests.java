4d1fa8a380862075ad1d9e473a5004f1c8697169
acsagum <acsagum.student@ua.edu.ph>
2024-05-22 17:12:51 +0800

    second commit

---
+++ car/src/test/java/com/sagum/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagum.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
